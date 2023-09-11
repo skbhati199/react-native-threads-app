f78fb80c4006db547b6faed1ef8313d75e215c44
sonu kumar <skbhati199@gmail.com>
2023-09-11 23:06:09 +0530

    add thread page and thread fake db

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,54 @@
-import { StyleSheet } from 'react-native';
+import {
+  Platform,
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import LottieView from "lottie-react-native";
+import { useContext, useRef } from "react";
+import { ThreadsContext } from "../../context/thread-context";
+import ThreadsItem from "../../components/ThreadItem";
+
 
 export default function TabOneScreen() {
+  const animationRef = useRef<LottieView>(null);
+
+  const threads = useContext(ThreadsContext);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView>
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 10,
+          paddingTop: Platform.select({ android: 30 }),
+        }}
+        refreshControl={
+          <RefreshControl
+            refreshing={false}
+            onRefresh={() => {
+              animationRef?.current?.play();
+            }}
+            tintColor={"transparent"}
+          />
+        }
+      >
+        <LottieView
+          ref={animationRef}
+          source={require("./../../json/animation_lmf2xgne.json")}
+          autoPlay
+          loop={false}
+          style={{
+            width: 90,
+            height: 90,
+            alignSelf: "center",
+          }}
+          // onAnimationFinish={()=> alert('Hello World')}
+        />
+        {threads.map((thread) => (
+          <ThreadsItem key={thread.id} thread={thread} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
